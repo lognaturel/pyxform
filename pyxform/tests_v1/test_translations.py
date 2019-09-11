9dc15cf3bfa3bd8e59ac71107c7805afe57266a5
Hélène Martin <lognaturel@gmail.com>
2019-09-11 09:58:37 -0700

    Add test that shows list index out of range error

--- pyxform/tests_v1/test_translations.py
+++ pyxform/tests_v1/test_translations.py
@@ -35,3 +35,23 @@ class DoubleColonTranslations(PyxformTestCase):
             xml__contains=["""<label ref="jr:itext('/translations/n1:label')"/>"""],
             model__contains=[model_contains],
         )
+
+    def test_translations_and_choice_filters(self):
+        self.assertPyxformXform(
+            name="translations_choice_filters",
+            id_string="transl",
+            md="""
+            | survey  |                    |          |                     |                    |
+            |         | type               | name     | label::English (en) | choice_filter      |
+            |         | select_one country | country  | Country             |                    |
+            |         | select_one city    | city     | City                | country=${country} |
+            | choices |                    |          |                     |                    |
+            |         | list_name          | name     | label               |                    |
+            |         | country            | france   | France              |                    |
+            |         | country            | canada   | Canada              |                    |
+            |         | city               | grenoble | Grenoble            |                    |
+            |         | city               | quebec   | Quebec              |                    |
+            """,
+            errored=False,
+            debug= True
+        )
