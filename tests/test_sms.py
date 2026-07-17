16d6ff621dfa78a94e4235bf315fe85fb54698bc
lindsay stevens <lstevens@getodk.org>
2026-07-17 23:59:08 +1000

    chg: replace sms_info xls test with equivalent md test
    - The deleted file was similar to the new test case in test_sms.py
      test_sms_info except that there were some additional items in the
      first group "a" and also a "meta" group with instance metadata (like
      phonenumber, imei).
      - As noted in the new test, there does not seem to be any special
        handling of any sms* fields in relation to the xform output. So the
        deleted test in test_builder.py was asserting that the sms* fields
        are read and stored internally, but for no apparent reason.
      - The new test at least demonstrates that old forms which may have
        these items will still convert without error - though whether they
        would still work as intended is unclear.

--- tests/test_sms.py
+++ tests/test_sms.py
@@ -62,3 +62,69 @@ class SMSTest(PyxformTestCase):
                 "<fruit/>",
             ],
         )
+
+    def test_sms_info(self):
+        md = """
+        | settings |
+        | | form_title | form_id  | sms_keyword | sms_separator | sms_allow_media | sms_date_format | sms_datetime_format |
+        | | SMS        | sms_info | inf         | +             | 1               | %Y-%m-%d        | %Y-%m-%d-%H:%M      |
+
+        | survey |
+        | | type                     | name         | sms_field | label                         |
+        | | begin_group              | section1     | a         |                               |
+        | | integer                  | age          | q1        | How old are you?              |
+        | | select_one yes_no        | has_children | q2        | Do you have any children?     |
+        | | end_group                |              |           |                               |
+        | | begin_group              | medias       | c         |                               |
+        | | image                    | picture      |           | May I take your picture?      |
+        | | geopoint                 | gps          |           | Record your GPS coordinates.  |
+        | | end_group                |              |           |                               |
+        | | begin_group              | browsers     | b         |                               |
+        | | select_multiple browsers | web_browsers | q5        | What web browsers do you use? |
+        | | end_group                |              |           |                               |
+
+        | choices |
+        | | list_name | name    | sms_option | label             |
+        | | yes_no    | n       | n          | no                |
+        | | yes_no    | y       | y          | yes               |
+        | | browsers  | firefox | ff         | Mozilla Firefox   |
+        | | browsers  | chrome  | gc         | Google Chrome     |
+        """
+
+        def get_cases(value) -> tuple[tuple[str, int], ...]:
+            """No element text and no attribute values match the input 'value'."""
+            return (
+                (f"""/h:html//*[text()='{value}']""", 0),
+                (f"""/h:html//*[@*='{value}']""", 0),
+            )
+
+        sms_values = (
+            "inf",
+            "+",
+            "1",
+            "%Y-%m-%d",
+            "%Y-%m-%d-%H:%M",
+            "a",
+            "q1",
+            "q2",
+            "b",
+            "c",
+        )
+
+        self.assertPyxformXform(
+            md=md,
+            xml__xpath_count=[
+                # The sms_* settings and sms_field values do not affect the xform output.
+                case
+                for val in sms_values
+                for case in get_cases(val)
+            ],
+            xml__xpath_match=[
+                # Model instance for choices contains sms_option values. Currently occurs
+                # due to passing through all choices data, not specific to sms_option.
+                """/h:html/h:head/x:model/x:instance[@id='yes_no']/x:root/x:item[./x:name='n' and ./x:sms_option='n']""",
+                """/h:html/h:head/x:model/x:instance[@id='yes_no']/x:root/x:item[./x:name='y' and ./x:sms_option='y']""",
+                """/h:html/h:head/x:model/x:instance[@id='browsers']/x:root/x:item[./x:name='firefox' and ./x:sms_option='ff']""",
+                """/h:html/h:head/x:model/x:instance[@id='browsers']/x:root/x:item[./x:name='chrome' and ./x:sms_option='gc']""",
+            ],
+        )
