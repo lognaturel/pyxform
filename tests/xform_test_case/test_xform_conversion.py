c3a08bca785acb32463716279480349305906fb3
lindsay stevens <lstevens@getodk.org>
2026-07-18 02:59:10 +1000

    chg: convert widgets and repeat_date_test from xls to xlsx
    - These files seem potentially useful to keep in some way:
      - widgets is a relatively large form with many different control
        and data types, appearances, and features.
      - repeat_date_test is less clearly useful except for an interesting
        serialisation scenario: a note label has a formatted date visible as
        "12/12/2012" in the spreadsheet but which is output as
        "2012-12-12 00:00:00" in the xform.

--- tests/xform_test_case/test_xform_conversion.py
+++ tests/xform_test_case/test_xform_conversion.py
@@ -18,9 +18,9 @@ class TestXFormConversion(XFormTestCase):
             ("flat_xlsform_test.xlsx", True),
             ("or_other.xlsx", True),
             ("pull_data.xlsx", True),
-            ("repeat_date_test.xls", True),
+            ("repeat_date_test.xlsx", True),
             ("survey_no_name.xlsx", False),
-            ("widgets.xls", True),
+            ("widgets.xlsx", True),
             ("xlsform_spec_test.xlsx", True),
         )
         for i, (case, set_name) in enumerate(cases):
